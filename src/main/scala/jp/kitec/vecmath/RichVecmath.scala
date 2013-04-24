866e95c3446d40758625b6c9dd67102c4fcf288e
Kensuke Matsuzaki <matsuzaki@kitec.co.jp>
2013-04-24 19:03:54 +0900

    Add float ver. Add ** as cross, and := as set.

--- src/main/scala/jp/kitec/vecmath/RichVecmath.scala
+++ src/main/scala/jp/kitec/vecmath/RichVecmath.scala
@@ -14,6 +14,9 @@ object RichVecmath {
   def Vector(x: Double, y: Double, z: Double) = new Vector3d(x, y, z)
 
   implicit class RichPoint3f(val self: Point3f) extends AnyVal {
+    def :=(that: Tuple3f) = {
+      self.set(that);
+    }
     def +=(that: Tuple3f) = {
       self.add(that);
     }
@@ -23,7 +26,6 @@ object RichVecmath {
     def *=(scale: Float) = {
       self.scale(scale);
     }
-
     def +(that: Vector3f) = {
       val v = new Point3f();
       v.add(self, that);
@@ -46,7 +48,46 @@ object RichVecmath {
     }
   }
 
+  implicit class RichPoint3d(val self: Point3d) extends AnyVal {
+    def :=(that: Tuple3d) = {
+      self.set(that);
+    }
+    def +=(that: Tuple3d) = {
+      self.add(that);
+    }
+    def -=(that: Tuple3d) = {
+      self.sub(that);
+    }
+    def *=(scale: Double) = {
+      self.scale(scale);
+    }
+
+    def +(that: Vector3d) = {
+      val v = new Point3d();
+      v.add(self, that);
+      v;
+    }
+    def -(that: Vector3d) = {
+      val v = new Point3d();
+      v.sub(self, that);
+      v;
+    }
+    def -(that: Point3d) = {
+      val v = new Vector3d();
+      v.sub(self, that);
+      v;
+    }
+    def *(scale: Double) = {
+      val v = new Point3d();
+      v.scale(scale, self);
+      v;
+    }
+  }
+
   implicit class RichVector3f(val self: Vector3f) extends AnyVal {
+    def :=(that: Tuple3f) = {
+      self.set(that);
+    }
     def +=(that: Tuple3f) = {
       self.add(that);
     }
@@ -71,10 +112,57 @@ object RichVecmath {
       v.sub(self, that);
       v;
     }
+    def **(that: Vector3f) = {
+      val v = new Vector3f();
+      v.cross(self, that);
+      v;
+    }
     def *(scale: Float) = {
       val v = new Vector3f();
       v.scale(scale, self);
       v;
     }
+    def *(that: Vector3f) = self.dot(that)
+  }
+
+  implicit class RichVector3d(val self: Vector3d) extends AnyVal {
+    def :=(that: Tuple3d) = {
+      self.set(that);
+    }
+    def +=(that: Tuple3d) = {
+      self.add(that);
+    }
+    def -=(that: Tuple3d) = {
+      self.sub(that);
+    }
+    def *=(scale: Double) = {
+      self.scale(scale);
+    }
+    def unary_- = {
+      val v = new Vector3d();
+      v.negate(self)
+      v;
+    }
+    def +(that: Vector3d) = {
+      val v = new Vector3d();
+      v.add(self, that);
+      v;
+    }
+    def -(that: Vector3d) = {
+      val v = new Vector3d();
+      v.sub(self, that);
+      v;
+    }
+    def **(that: Vector3d) = {
+      val v = new Vector3d();
+      v.cross(self, that);
+      v;
+    }
+    def *(scale: Double) = {
+      val v = new Vector3d();
+      v.scale(scale, self);
+      v;
+    }
+    def *(that: Vector3d) = self.dot(that)
   }
 }
--- src/test/scala/jp/kitec/vecmath/RichVecmathSpec.scala
+++ src/test/scala/jp/kitec/vecmath/RichVecmathSpec.scala
@@ -11,11 +11,11 @@ import org.specs2.runner._
 
 @RunWith(classOf[JUnitRunner])
 class RichVecmathSpec extends Specification {
-  "Vector3f" should {
+  "Point3f" should {
     val a = Point(1, 2, 3);
     val b = Vector(5, 7, 9);
     val c = Point(5, 7, 9);
-    "operator +" in {
+    "point + vector" in {
       a + b must_== Point(6, 9, 12)
     }
     "point - vector" in {
@@ -31,7 +31,7 @@ class RichVecmathSpec extends Specification {
         -Vector(1, 2, 3) + Vector(2, 5, 0) * 2 must_== Vector(3, 8, -3)
     }
   }
-  "Point3f" should {
+  "Vector3f" should {
     val a = new Vector3f(1, 2, 3);
     val b = new Vector3f(5, 7, 9);
     "vector + vector" in {
@@ -42,6 +42,14 @@ class RichVecmathSpec extends Specification {
     }
     "vector * float" in {
         a * 3 must_== Vector(3, 6, 9);
+    }
+    "vector ** float" in {
+        Vector(1f, 0f, 0f) ** Vector(0f, 1f, 0f) must_== Vector(0f, 0f, 1f);
+    }
+    "vector := vector" in {
+      val e = Vector(1f, 2f, 3f);
+      e := Vector(2f, 3f, 4f)
+      e must_== Vector(2f, 3f, 4f)
     }
     "expression" in {
         Point(1, 2, 3) + Vector(2, 5, 0) * 4 must_== Point(9, 22, 3)
